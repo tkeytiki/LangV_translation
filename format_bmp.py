a7e3343c1e240e753dd7e46f73d7935921d6a3c8
tkeytiki <whopointseven@gmail.com>
2024-08-04 21:38:14 -0400

    fix upside down

--- format_bmp.py
+++ format_bmp.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 numpy.set_printoptions(threshold=sys.maxsize)
 
+bytes = []
+
 with open("font\\psx6x12dualtry.bmp", mode="rb") as i:
     i.seek(0x0A) #offset that contains start addr of pixel array
     offset = i.read(1)[0]
@@ -14,20 +16,24 @@ with open("font\\psx6x12dualtry.bmp", mode="rb") as i:
 
 bytes = numpy.fromfile("font\\noheader.bin", dtype="uint8")
 
-#bmp image data is stored upside down, have to flip it over each axis
+#bmp image data is stored upside down, have to flip it rightside up
 
-bits = numpy.unpackbits(bytes)[::-1] #vertical flip
+bits = numpy.unpackbits(bytes)[::-1] #flips over both axes
 
 newbits = list()
 
 for r in range(int(len(bits)/2304)):
+    row = list()
     for x in range(16):
+        char = list()
         for y in range(12):
-            row = list()
+            charrow = list()
             for xy in range(r*2304+x*12+y*192, r*2304+x*12+12+y*192):
                 #newbits.append(bits[xy])
-                row.append(bits[xy]) #form a pixel row
-            newbits.append(row[::-1]) #horizontal flip on the row
+                charrow.append(bits[xy]) #add a pixel to a row of the char
+            char.append(charrow[::-1]) #add a row of the char to the char, flipped horizontally
+        row.append(char) #add the completed char to the char row
+    newbits.append(row[::-1]) #flip row horizontally
 
 #x is column, y is row
 #r is CHAR row
