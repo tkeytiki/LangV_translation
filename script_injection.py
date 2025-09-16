1c92af152f4642320a8303b130c20b3202a48816
terry <whopointseven@gmail.com>
2025-09-16 19:57:13 -0400

    now correctly overwrites all data of first scenario

--- script_injection.py
+++ script_injection.py
@@ -31,7 +31,8 @@ with open("gamefiles\\input\\SCEN.DAT", mode="rb") as origin, \
 
     numlines = 0
     trans_hex = ""
-    offset_table = ""
+    offset_table = int.to_bytes(offset, 2, 'little').hex()
+
 
     while trans:
         numlines += 1
@@ -45,13 +46,14 @@ with open("gamefiles\\input\\SCEN.DAT", mode="rb") as origin, \
 
         new_offset = offset + l #calculate the next offset value
         new_offset = int.to_bytes(new_offset, 2, 'little') #convert to bytes
-        offset_table += new_offset.hex()
+
 
         trans = eng.readline()
 
         if offset_addr != first_offset+scenario.script_pointer:
             # stop it from writing another offset when it reaches the beginning of script
             # aka offset_addr = first offset in pointer table
+            offset_table += new_offset.hex()
             new.seek(offset_addr)
             new.write(new_offset) #write new offset
             #print(f'new offset: {new_offset.hex()}')
@@ -71,8 +73,8 @@ with open("gamefiles\\input\\SCEN.DAT", mode="rb") as origin, \
     # convert script length to bytes and write
 
     new.seek(scenario.script_pointer)
-    new.write(new_script_len.to_bytes(4, byteorder='little'))
-
+    new_script_len = new_script_len.to_bytes(4, byteorder='little')
+    new.write(new_script_len)
 
     # push back the data that follows the script
     # -> change to appending the data that follows the script
@@ -81,30 +83,38 @@ with open("gamefiles\\input\\SCEN.DAT", mode="rb") as origin, \
     origin.seek(post_script_offset)
     post_script_data = origin.read(dr.Scenario.pointers[scenario.scenario_number + 1] - post_script_offset)
 
+    offset_table = new_script_len.hex() + offset_table
+
     trans_hex += post_script_data.hex()
     trans_hex = offset_table + trans_hex
     trans_hex = bytearray.fromhex(trans_hex)
-
     scenario.add_script(trans_hex)
     scenario.repoint_next()
 
-    with open("gamefiles\\output\\SCEN1.BIN", mode="wb") as newdata:
+    with open("gamefiles\\output\\SCEN.DAT", mode="wb") as newdata:
         origin.seek(0)
         newdata.write(origin.read())
+        newdata.seek(scenario.addr)
+        erase = bytearray(dr.Scenario.pointers[scenario.scenario_number+1] - scenario.addr)
+        newdata.write(erase)
         newdata.seek(dr.Scenario.new_pointers[scenario.scenario_number])
         newdata.write(scenario.data)
         newdata.write(scenario.script)
-    #zero_search = origin.read(32)
-    #data = zero_search
+        newdata.seek(0)
+        newdata.write(dr.Scenario.new_pointers[scenario.scenario_number].to_bytes(length=4, byteorder="little"))
 
-    #while zero_search != bytearray(32):
-    #    zero_search = origin.read(32)
-    #    data += zero_search
+    '''
+    zero_search = origin.read(32)
+    data = zero_search
 
-    #end_of_data = origin.tell()
+    while zero_search != bytearray(32):
+        zero_search = origin.read(32)
+        data += zero_search
+
+    end_of_data = origin.tell()
 
     # remove trailing 00 from data
-    '''
+
     print(data.hex().rstrip('0'))
     data = data.hex().rstrip('0')
     if len(data) % 2 != 0:
@@ -113,9 +123,10 @@ with open("gamefiles\\input\\SCEN.DAT", mode="rb") as origin, \
     print(data)
     print(f'new script len: {hex(new_script_len)}')
     print(f'length diff: {new_script_len - original_script_len}')
+
+    new.seek(scenario.script_pointer + new_script_len)
+    new.write(data)
     '''
-    #new.seek(scenario.script_pointer + new_script_len)
-    #new.write(data)
 
     #oops, script length needs to include the length of the pointer offset table itself
 
