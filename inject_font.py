306e63ffab59760513dc88be220bdf3508257504
terry <whopointseven@gmail.com>
2025-10-02 19:25:03 -0400

    new function to inject non-dual font

--- inject_font.py
+++ inject_font.py
@@ -9,3 +9,11 @@ def inject_font():
         systr = syst.read()
         newsys.write(font+systr)
 
+def inject_font_oneletter():
+    with open("font\\oneletterfont.bin", mode="rb") as f, \
+            open("gamefiles\\input\\SYSTEM.BIN", mode="rb") as syst, \
+            open("gamefiles\\output\\SYSTEM.BIN", mode="wb") as newsys:
+        font = f.read()
+        syst.seek(int(len(font)))
+        systr = syst.read()
+        newsys.write(font+systr)
