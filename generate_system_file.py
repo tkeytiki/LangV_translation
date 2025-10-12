d2604cdd85748112f6bb363c6cbc4df3a0e1227a
terry <whopointseven@gmail.com>
2025-10-12 01:06:46 -0400

    added font extension function and B8 character

--- generate_system_file.py
+++ generate_system_file.py
@@ -1,5 +1,5 @@
 import format_bmp as fb
 import inject_font as inf
 
-fb.format_bmp()
+fb.create_font_extension()
 inf.inject_font_oneletter()
--- inject_font.py
+++ inject_font.py
@@ -11,9 +11,15 @@ def inject_font():
 
 def inject_font_oneletter():
     with open("font\\oneletterfont.bin", mode="rb") as f, \
+            open("font\\fontextension.bin", mode="rb") as fe, \
             open("gamefiles\\input\\SYSTEM.BIN", mode="rb") as syst, \
             open("gamefiles\\output\\SYSTEM.BIN", mode="wb") as newsys:
         font = f.read()
+        font += fe.read()
+        print(font)
         syst.seek(int(len(font)))
         systr = syst.read()
         newsys.write(font+systr)
+        #insert blank space for B8 "character"
+        newsys.seek(0xb8*18)
+        newsys.write(bytearray(18)) #write 144 blank 0 bits
