b95914424557ef847a37dc24566080547fd5f414
terry <whopointseven@gmail.com>
2025-10-15 14:17:13 -0400

    remove prints

--- system_block.py
+++ system_block.py
@@ -9,7 +9,6 @@ class Block:
     def from_untranslated_block(self, offsets, lines):
         i = 0
         self.offsets.pop()
-        print(f"offsetlen:{len(offsets)}")
         while i < len(offsets):
             self.offsets.append(offsets[i:i+2])
             i += 2
@@ -27,7 +26,6 @@ class Block:
 
     def add_line(self, line):
         self.lines_len += len(line)//2
-        print(self.lines_len)
         self.lines += line
         ll = self.lines_len//2 #divide offset by two because for some reason system pointers are multiplied
         #by 2 to get the real offset
